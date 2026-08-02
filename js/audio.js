554a7e5f8af46abd7175e6ff204f2c5b5f9e5e14
Nesio Ferreira <nesio_asa@hotmail.com>
2026-08-02 18:37:38 -0300

    Refactor AudioManager methods to use 'function' syntax

--- js/audio.js
+++ js/audio.js
@@ -1,20 +1,27 @@
 // ============================================================
-//  AUDIO - Gerenciador de sons
+//  AUDIO - Gerenciador de sons (Web Audio API)
 // ============================================================
-const AudioManager = {
+var AudioManager = {
     ctx: null,
 
-    init() {
+    init: function() {
         if (!this.ctx) {
-            this.ctx = new (window.AudioContext || window.webkitAudioContext)();
+            try {
+                this.ctx = new (window.AudioContext || window.webkitAudioContext)();
+            } catch (e) {
+                // Silêncio em navegadores sem suporte
+            }
         }
     },
 
-    play(freq, duration, type = 'sine', volume = 0.3) {
+    play: function(freq, duration, type, volume) {
+        type = type || 'sine';
+        volume = volume || 0.3;
         try {
             this.init();
-            const osc = this.ctx.createOscillator();
-            const gain = this.ctx.createGain();
+            if (!this.ctx) return;
+            var osc = this.ctx.createOscillator();
+            var gain = this.ctx.createGain();
             osc.type = type;
             osc.frequency.value = freq;
             gain.gain.setValueAtTime(volume, this.ctx.currentTime);
@@ -23,41 +30,39 @@ const AudioManager = {
             gain.connect(this.ctx.destination);
             osc.start();
             osc.stop(this.ctx.currentTime + duration);
-        } catch (e) { /* silêncio */ }
+        } catch (e) {}
     },
 
-    pickup() {
-        this.play(800, 0.15, 'sine', 0.2);
-        setTimeout(() => this.play(1000, 0.15, 'sine', 0.2), 100);
+    pickup: function() {
+        var self = this;
+        this.play(800, 0.15);
+        setTimeout(function() { self.play(1000, 0.15); }, 100);
     },
 
-    hit() {
+    hit: function() {
         this.play(200, 0.3, 'sawtooth', 0.2);
     },
 
-    victory() {
+    victory: function() {
+        var self = this;
         this.play(523, 0.2, 'sine', 0.3);
-        setTimeout(() => this.play(659, 0.2, 'sine', 0.3), 150);
-        setTimeout(() => this.play(784, 0.4, 'sine', 0.3), 300);
+        setTimeout(function() { self.play(659, 0.2, 'sine', 0.3); }, 150);
+        setTimeout(function() { self.play(784, 0.4, 'sine', 0.3); }, 300);
     },
 
-    heartbeat() {
+    heartbeat: function() {
         this.play(60, 0.1, 'sine', 0.1);
     },
 
-    boss() {
+    boss: function() {
+        var self = this;
         this.play(440, 0.3, 'square', 0.15);
-        setTimeout(() => this.play(554, 0.3, 'square', 0.15), 200);
+        setTimeout(function() { self.play(554, 0.3, 'square', 0.15); }, 200);
     },
 
-    doorOpen() {
+    doorOpen: function() {
+        var self = this;
         this.play(600, 0.2, 'sine', 0.15);
-        setTimeout(() => this.play(800, 0.2, 'sine', 0.15), 100);
-    },
-
-    complete() {
-        [523, 587, 659, 784].forEach((freq, i) => {
-            setTimeout(() => this.play(freq, 0.3, 'sine', 0.25), i * 150);
-        });
+        setTimeout(function() { self.play(800, 0.2, 'sine', 0.15); }, 100);
     }
 };
